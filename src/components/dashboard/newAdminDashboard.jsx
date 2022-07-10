24bce4b8da88d26376d8588929631bcd08683cec
Josh McLain <joshmclain45@gmail.com>
2022-07-10 15:09:27 -0700

    render patient list to dash

--- src/components/dashboard/newAdminDashboard.jsx
+++ src/components/dashboard/newAdminDashboard.jsx
@@ -1,13 +1,44 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { getAllPatients } from '../../redux/actions/patientActions';
 
+import { DashboardContainer } from './style';
+
 const NewAdminDashboard = () => {
+  const patientsList = useSelector(({ patients: { list }}) => list);
   const dispatch = useDispatch();
+
   useEffect(() => {
     dispatch(getAllPatients());
   }, [dispatch]);
-  return <div>newAdminDashboard</div>;
+
+
+
+  return (
+    <DashboardContainer>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>ahcccs id</th>
+            <th>phone #</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patientsList.map((patient, index) => {
+            const { firstName, lastName, phoneNumber, ahcccsId } = patient;
+            return (
+              <tr>
+                <td>{`${firstName} ${lastName}`}</td>
+                <td>{ahcccsId}</td>
+                <td>{phoneNumber}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </DashboardContainer>
+  );
 };
 
 export default NewAdminDashboard;
