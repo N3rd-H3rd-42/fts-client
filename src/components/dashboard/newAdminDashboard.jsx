37f22245516ea96402c8b7604b93820d988c79bc
Josh McLain <joshmclain45@gmail.com>
2022-07-11 22:31:34 -0700

    new patient modal form ui

--- src/components/dashboard/newAdminDashboard.jsx
+++ src/components/dashboard/newAdminDashboard.jsx
@@ -16,8 +16,11 @@ import {
   NewPatientModalContainer,
   ModalContentWrapper,
   ModalHeader,
-  ModalBody,
+  ModalFormBody,
   ModalFooter,
+  FormGroupRow,
+  InputLabel,
+  InputElement,
 } from './style';
 
 const NewAdminDashboard = () => {
@@ -39,7 +42,7 @@ const NewAdminDashboard = () => {
   return (
     <>
       {isModalOpen && (
-        <NewPatientModalContainer>
+        <NewPatientModalContainer className="new-patient-modal">
           <ModalContentWrapper>
             <ModalHeader>
               <div>
@@ -49,9 +52,55 @@ const NewAdminDashboard = () => {
                 <Button onClick={toggleNewPatientModal}>X</Button>
               </div>
             </ModalHeader>
-            <ModalBody>
-            </ModalBody>
+            <ModalFormBody>
+              <FormGroupRow width={33.3}>
+                <InputLabel>First name:</InputLabel>
+                <InputElement type="text" name="firstName" />
+              </FormGroupRow>
+              <FormGroupRow width={33.4}>
+                <InputLabel>Last name:</InputLabel>
+                <InputElement type="text" name="lastName" />
+              </FormGroupRow>
+              <FormGroupRow width={33.3}>
+                <InputLabel>ACCCHS ID:</InputLabel>
+                <InputElement type="text" name="accchsId" />
+              </FormGroupRow>
+              <FormGroupRow width={100}>
+                <InputLabel>Pickup location name:</InputLabel>
+                <InputElement type="text" name="locationName" />
+              </FormGroupRow>
+              <FormGroupRow width={50}>
+                <InputLabel>Pickup address</InputLabel>
+                <InputElement type="text" name="locationAddress1" />
+              </FormGroupRow>
+              <FormGroupRow width={50}>
+                <InputLabel>City</InputLabel>
+                <InputElement type="text" name="city" />
+              </FormGroupRow>
+              <FormGroupRow width={50}>
+                <InputLabel>address 2</InputLabel>
+                <InputElement type="text" name="locationAddress2" />
+              </FormGroupRow>
+              <FormGroupRow width={50}>
+                <InputLabel>zip code</InputLabel>
+                <InputElement type="text" name="zipCode" />
+              </FormGroupRow>
+              <FormGroupRow width={50}>
+                <InputLabel>contact phone number</InputLabel>
+                <InputElement type="text" name="phoneNumber" />
+              </FormGroupRow>
+              <FormGroupRow width={50}>
+                <InputLabel>Prefferd Driver</InputLabel>
+                <InputElement type="text" name="prefferedDriver" />
+              </FormGroupRow>
+            </ModalFormBody>
             <ModalFooter>
+              <span>
+                <Button>Cancel</Button>
+              </span>
+              <span>
+                <Button>Submit</Button>
+              </span>
             </ModalFooter>
           </ModalContentWrapper>
         </NewPatientModalContainer>
--- src/components/dashboard/style.js
+++ src/components/dashboard/style.js
@@ -33,28 +33,47 @@ export const BodyCell = styled.td``;
 export const Button = styled.button``;
 
 export const NewPatientModalContainer = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-    background: rgba(0, 0, 0, 0.6);
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.6);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const ModalContentWrapper = styled.div`
-    background: #fff;
-    width: 50%;
-    height: 50%;
+  background: #fff;
+  max-width: 50%;
+  min-height: 50%;
+  padding: 18px 8px 50px;
 `;
 
 export const ModalHeader = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-`
-export const ModalBody = styled.div``;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+`;
+export const ModalFormBody = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+`;
 
-export const ModalFooter = styled.div``;
+export const ModalFooter = styled.div`
+    text-align: center;
+    margin-top: 36px;
+`;
+
+export const FormGroupRow = styled.div`
+  width: ${({ width }) => (width ? `${width}%` : '100%')};
+  padding: 12px 10px;
+`;
+
+export const InputLabel = styled.span``;
+
+export const InputElement = styled.input`
+  width: 100%;
+`;
