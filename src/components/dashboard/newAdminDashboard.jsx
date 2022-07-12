60140ff07746e7b3e533411e08b5cee5219a07e9
Josh McLain <joshmclain45@gmail.com>
2022-07-11 19:41:36 -0700

    new patient modal ui

--- src/components/dashboard/newAdminDashboard.jsx
+++ src/components/dashboard/newAdminDashboard.jsx
@@ -1,48 +1,94 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getAllPatients } from '../../redux/actions/patientActions';
 
 import {
   DashboardContainer,
+  LeftSideNavWrapper,
+  MainCenterWrapper,
+  RightSideDetailsWrapper,
   Table,
   TableHead,
   TableBody,
   BodyRow,
   BodyCell,
+  Button,
+  NewPatientModalContainer,
+  ModalContentWrapper,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
 } from './style';
 
 const NewAdminDashboard = () => {
-  const patientsList = useSelector(({ patients: { list } }) => list);
   const dispatch = useDispatch();
+
+  const patientsList = useSelector(({ patients: { list } }) => list);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleNewPatientModal = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsModalOpen(!isModalOpen);
+  };
 
   useEffect(() => {
     dispatch(getAllPatients());
   }, [dispatch]);
 
   return (
-    <DashboardContainer>
-      <Table>
-        <TableHead>
-          <tr>
-            <th>name</th>
-            <th>ahcccs id</th>
-            <th>phone #</th>
-          </tr>
-        </TableHead>
-        <TableBody>
-          {patientsList.map((patient, index) => {
-            const { firstName, lastName, phoneNumber, ahcccsId } = patient;
-            return (
-              <BodyRow key={index}>
-                <BodyCell>{`${firstName} ${lastName}`}</BodyCell>
-                <BodyCell>{ahcccsId}</BodyCell>
-                <BodyCell>{phoneNumber}</BodyCell>
-              </BodyRow>
-            );
-          })}
-        </TableBody>
-      </Table>
-    </DashboardContainer>
+    <>
+      {isModalOpen && (
+        <NewPatientModalContainer>
+          <ModalContentWrapper>
+            <ModalHeader>
+              <div>
+                <h3>Add new patient</h3>
+              </div>
+              <div>
+                <Button onClick={toggleNewPatientModal}>X</Button>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+            </ModalBody>
+            <ModalFooter>
+            </ModalFooter>
+          </ModalContentWrapper>
+        </NewPatientModalContainer>
+      )}
+      <DashboardContainer>
+        <LeftSideNavWrapper>
+          <h1>Coming soon?</h1>
+          <Button onClick={toggleNewPatientModal}>Add new patient</Button>
+        </LeftSideNavWrapper>
+        <MainCenterWrapper>
+          <Table>
+            <TableHead>
+              <tr>
+                <th>name</th>
+                <th>ahcccs id</th>
+                <th>phone #</th>
+              </tr>
+            </TableHead>
+            <TableBody>
+              {patientsList.map((patient, index) => {
+                const { firstName, lastName, phoneNumber, ahcccsId } = patient;
+                return (
+                  <BodyRow key={index}>
+                    <BodyCell>{`${firstName} ${lastName}`}</BodyCell>
+                    <BodyCell>{ahcccsId}</BodyCell>
+                    <BodyCell>{phoneNumber}</BodyCell>
+                  </BodyRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </MainCenterWrapper>
+        <RightSideDetailsWrapper>
+          <h3>details</h3>
+        </RightSideDetailsWrapper>
+      </DashboardContainer>
+    </>
   );
 };
 
--- src/components/dashboard/style.js
+++ src/components/dashboard/style.js
@@ -1,7 +1,23 @@
 import styled from 'styled-components';
 
 export const DashboardContainer = styled.div`
-    min-height: 50vh;
+  min-height: 50vh;
+  display: flex;
+`;
+
+export const LeftSideNavWrapper = styled.div`
+  border: 1px solid green;
+  width: 15%;
+`;
+
+export const MainCenterWrapper = styled.div`
+  border: 1px solid purple;
+  width: 50%;
+`;
+
+export const RightSideDetailsWrapper = styled.div`
+  border: 1px solid orange;
+  width: 35%;
 `;
 
 export const Table = styled.table``;
@@ -13,3 +29,32 @@ export const TableBody = styled.tbody``;
 export const BodyRow = styled.tr``;
 
 export const BodyCell = styled.td``;
+
+export const Button = styled.button``;
+
+export const NewPatientModalContainer = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(0, 0, 0, 0.6);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const ModalContentWrapper = styled.div`
+    background: #fff;
+    width: 50%;
+    height: 50%;
+`;
+
+export const ModalHeader = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+export const ModalBody = styled.div``;
+
+export const ModalFooter = styled.div``;
