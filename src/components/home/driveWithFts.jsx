2417e161eeef5e8ab62e497f09410e7975420eb1
Mark Clark <markfitnessaz@gmail.com>
2022-08-22 12:43:37 -0700

    updated home bottom banner image and content

--- src/components/home/driveWithFts.jsx
+++ src/components/home/driveWithFts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const DriveWithFts = () => {
-  
+
   const handleClick = () => {
     window.location = "/apply";
     return;
@@ -11,24 +11,27 @@ const DriveWithFts = () => {
     <div id="drive-with-fts">
       <div className="drive-with-fts-overlay"></div>
 
-      <div className="drive-with-fts-container">
-        <h1>DRIVE WITH FTS</h1>
-        <h3>
-          Drive with FTS, we are safe, fast and reliable. Partners across
-          Arizona are now riding with FTS.
-        </h3>
+      <div className="drive-with-fts-main-content">
+        <div id="drive-with-fts-side-img"></div>
+        <div className="drive-with-fts-container">
+          <h1>DRIVE WITH FTS</h1>
+          <h3>
+            Drive with FTS, we are safe, fast and reliable. Partners across
+            Arizona are now riding with FTS.
+          </h3>
 
-        <p>
-          Future Trans Systems is always looking for safe and reliable drivers. Applying is easy. Join our dynamic and flexible team by following the link below and completing our online application. Our team will help you apply for required credentials and get you on the road quickly.
-        </p>
+          <p>
+            Future Trans Systems is always looking for safe and reliable drivers. Applying is easy. Join our dynamic and flexible team by following the link below and completing our online application. Our team will help you apply for required credentials and get you on the road quickly.
+          </p>
 
-        <button onClick={handleClick} className="home-apply-btn">
-          APPLY
-        </button>
+          <button onClick={handleClick} className="home-apply-btn">
+            APPLY
+          </button>
+        </div>
       </div>
 
       <div className="bottom-overlay">
-        <p>
+        <p className="bottom-overlay-p">
           Future Trans Systems puts our employees and customers safety first.
           That is why we are trusted by more than 130 medical facilities and
           clinic in Arizona and abroad.
