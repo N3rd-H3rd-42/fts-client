c556fb11636ebd0a0f2bda21dd8e334806ed4a39
Josh McLain <joshmclain45@gmail.com>
2022-08-28 23:48:13 -0700

    FTS-9: connect patient ui to back end

--- src/redux/actions/patientActions.js
+++ src/redux/actions/patientActions.js
@@ -5,6 +5,8 @@ export const FETCH_ALL_PATIENTS = 'FETCH_ALL_PATIENTS';
 export const CREATE_NEW_PATIENT = 'CREATE_NEW_PATIENT';
 export const FETCH_PATIENT_DETAILS = 'FETCH_PATIENT_DETAILS';
 export const UPDATE_PATIENT_DETAILS = 'UPDATE_PATIENT_DETAILS';
+export const TOGGLE_PATIENT_ACTIVE = 'TOGGLE_PATIENT_ACTIVE';
+export const DELETE_PATIENT = 'DELETE_PATIENT';
 
 const { REACT_APP_BACKEND_URL } = process.env;
 
@@ -24,10 +26,15 @@ export const getAllPatients = () => async (dispatch) => {
 
 export const createNewPatient = (body) => async (dispatch) => {
   try {
-    const response = await axios.post(`${REACT_APP_BACKEND_URL}api/patient`, body);
+    const response = await axios.post(
+      `${REACT_APP_BACKEND_URL}api/patient`,
+      body
+    );
     if (response.status === 201) {
       dispatch({ type: CREATE_NEW_PATIENT });
-      const updatedListRes = await axios.get(`${REACT_APP_BACKEND_URL}api/patients`);
+      const updatedListRes = await axios.get(
+        `${REACT_APP_BACKEND_URL}api/patients`
+      );
       if (updatedListRes.status === 200)
         dispatch({
           type: FETCH_ALL_PATIENTS,
@@ -42,7 +49,9 @@ export const createNewPatient = (body) => async (dispatch) => {
 
 export const getSinglePatientDetails = (patientId) => async (dispatch) => {
   try {
-    const response = await axios.get(`${REACT_APP_BACKEND_URL}api/patient/${patientId}`);
+    const response = await axios.get(
+      `${REACT_APP_BACKEND_URL}api/patient/${patientId}`
+    );
     if (response.status === 200)
       dispatch({ type: FETCH_PATIENT_DETAILS, payload: response.data?.data });
   } catch (error) {
@@ -63,7 +72,9 @@ export const updatePatientDetails =
           type: UPDATE_PATIENT_DETAILS,
           payload: response.data?.data,
         });
-        const updatedListRes = await axios.get(`${REACT_APP_BACKEND_URL}api/patients`);
+        const updatedListRes = await axios.get(
+          `${REACT_APP_BACKEND_URL}api/patients`
+        );
         if (updatedListRes.status === 200)
           dispatch({
             type: FETCH_ALL_PATIENTS,
@@ -75,3 +86,38 @@ export const updatePatientDetails =
       throw error;
     }
   };
+
+export const togglePatientIsActive = (patientId) => async (dispatch) => {
+  try {
+    const response = await axios.put(
+      `${REACT_APP_BACKEND_URL}api/patient/toggle-active/${patientId}`
+    );
+    if (response.status === 200)
+      dispatch({ type: TOGGLE_PATIENT_ACTIVE, payload: response.data?.data });
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const deletePatient = (patientId) => async (dispatch) => {
+  try {
+    const response = await axios.delete(
+      `${REACT_APP_BACKEND_URL}api/patient/${patientId}`
+    );
+    if (response.status === 200) {
+      dispatch({ type: DELETE_PATIENT });
+      const updatedListRes = await axios.get(
+        `${REACT_APP_BACKEND_URL}api/patients`
+      );
+      if (updatedListRes.status === 200)
+        dispatch({
+          type: FETCH_ALL_PATIENTS,
+          payload: updatedListRes.data?.patientList,
+        });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
--- src/redux/reducers/patientsReducer.js
+++ src/redux/reducers/patientsReducer.js
@@ -2,7 +2,9 @@ import {
   FETCH_ALL_PATIENTS,
   CREATE_NEW_PATIENT,
   FETCH_PATIENT_DETAILS,
-  UPDATE_PATIENT_DETAILS
+  UPDATE_PATIENT_DETAILS,
+  TOGGLE_PATIENT_ACTIVE,
+  DELETE_PATIENT,
 } from '../actions/patientActions';
 
 const initialState = {
@@ -19,11 +21,18 @@ export const patientsReducer = (state = initialState, { type, payload }) => {
       };
     case FETCH_PATIENT_DETAILS:
     case UPDATE_PATIENT_DETAILS:
+    case TOGGLE_PATIENT_ACTIVE:
       return {
         ...state,
         selectedPatient: { ...payload },
       };
     case CREATE_NEW_PATIENT:
+      return { ...state };
+    case DELETE_PATIENT:
+      return {
+        ...state,
+        selectedPatient: {},
+      };
     default:
       return state;
   }
